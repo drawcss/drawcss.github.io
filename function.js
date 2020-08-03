85ec65f06ae2f68389d9eac1d77b3362e3e8e786
Cricket <39403108+drawcss@users.noreply.github.com>
2020-08-03 11:57:48 +0700

    ReDesign
    Redesign portfolio 03/08

--- function.js
+++ function.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded",function(){
 	var articlesLink = document.querySelectorAll(".articles-link");
 	var wrapAll = document.querySelectorAll(".wrap");
 
+	var tag = document.querySelectorAll(".tag");
+	var wrapArticles = document.querySelectorAll(".articles-wrap");
+
 	// window.addEventListener("load", function(){
 	// 	var loadScreen = document.querySelector(".load-screen");
 	// 	document.body.removeChild(loadScreen);
@@ -24,6 +27,43 @@ document.addEventListener("DOMContentLoaded",function(){
 		}
 
 	}
+
+	for (let i = 0; i < tag.length; i++) {
+		tag[i].onclick = function() {
+			let keyTag = this.innerHTML.substr(1);
+			console.log(keyTag);
+			for (let j = 0; j < tag.length; j++) {
+				tag[j].classList.remove("is-tag-selected");
+			}
+			tag[i].classList.add("is-tag-selected");
+
+			// convert html to the same with class
+
+			for (let i = 0; i < wrapArticles.length; i++) {
+				wrapArticles[i].classList.add("hide-tag-articles");
+				if(keyTag === "all") {
+					wrapArticles[i].classList.remove("hide-tag-articles");
+				}
+				else if(wrapArticles[i].classList.contains(keyTag)) {
+					wrapArticles[i].classList.remove("hide-tag-articles");
+				}
+			}
+			
+			
+		}
+	}
+
+
+
+	// for (let i = 0; i < bannerSlide.length; i++) {
+	// 	bannerSlide.eq(i).on("click", function(){
+	// 		for (let j = 0; j < bannerSlide.length; j++) {
+	// 			bannerSlide.eq(j).removeClass("line-beside");
+	// 		}
+	// 		bannerSlide.eq(i).addClass("line-beside");
+	// 	});
+	// }
+
 
 }, false);
 
